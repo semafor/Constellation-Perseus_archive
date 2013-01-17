ff6016688837ab81aa0ce96f8d3160fc073bf263
Jonas G. Drange <jonas@drange.net>
2013-01-17 23:17:36 +0100

    some formatting fixes

--- planetary_name_generator.py
+++ planetary_name_generator.py
@@ -17,22 +17,22 @@ def generate_name():
         name = "%s %s-%s" % (get_number(), get_name(), get_name())
 
     elif(r > 40 and r <= 50):
-        name = "%s (%s) %s" % (get_name(), get_name(), get_number())
+        name = "%s %s %s" % (get_name(), get_letter(), get_number())
 
     elif(r > 50 and r <= 60):
-        name = "%s %s (%s)" % (get_number(), get_name(), get_name())
+        name = "%s %s %s" % (get_number(), get_name(), get_letter())
 
     elif(r > 60 and r <= 70):
-        name = "%s.%s %s" % (get_number(), get_number(), get_name())
+        name = "%s-%s %s" % (get_number(), get_letter(), get_name())
 
     elif(r > 70 and r <= 80):
-        name = "%s %s.%s" % (get_name(), get_number(), get_number())
+        name = "%s %s-%s" % (get_name(), get_letter(), get_number())
 
     elif(r > 80 and r <= 90):
-        name = "%s.%s %s-%s" % (get_number(), get_number(), get_name(), get_name())
+        name = "%s-%s %s" % (get_name(), get_letter(), get_number())
 
     elif(r > 90 and r <= 100):
-        name = "%s-%s %s.%s" % (get_name(), get_name(), get_number(), get_number())
+        name = "%s-%s %s" % (get_letter(), get_number(), get_name())
 
     return name
 
@@ -43,6 +43,15 @@ def get_number():
 
 def get_name():
     return names[randint(0, (len(names) - 1))]
+
+def get_letter():
+    letters = "ABCDEFGXZ"
+    letter = letters[randint(0, (len(letters) - 1))]
+
+    if randint(0, 1):
+        letter = letter.lower()
+
+    return letter
 
 
 names = [
@@ -69,4 +78,5 @@ names = [
     "Leonis",
     "Virginis",
     "Cancri",
+    "PLA",
 ]
